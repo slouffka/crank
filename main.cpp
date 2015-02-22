9755c5650c6569cbfb79cfc249f7ac65ddb27a04
Vyacheslav Moskalev <slouffka@gmail.com>
2015-02-23 04:53:00 +0600

    Added keyboard controls and frame independent movement

--- main.cpp
+++ main.cpp
@@ -8,17 +8,32 @@ class Game
 
     private:
         void                processEvents();
-        void                update();
+        void                update(sf::Time frameTime);
         void                render();
 
+        void                handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
     private:
+        static const float  PlayerSpeed;
+
         sf::RenderWindow    mWindow;
         sf::CircleShape     mPlayer;
+
+        bool                mIsMovingUp;
+        bool                mIsMovingDown;
+        bool                mIsMovingLeft;
+        bool                mIsMovingRight;
 };
 
+const float Game::PlayerSpeed = 250.f;
+
 Game::Game()
-: mWindow(sf::VideoMode(640, 480), "Crank Game")
+: mWindow(sf::VideoMode(640, 480), "Crank Game", sf::Style::Close)
 , mPlayer()
+, mIsMovingUp(false)
+, mIsMovingDown(false)
+, mIsMovingLeft(false)
+, mIsMovingRight(false)
 {
     mPlayer.setRadius(40.f);
     mPlayer.setPosition(100.f, 100.f);
@@ -27,10 +42,13 @@ Game::Game()
 
 void Game::run()
 {
+    sf::Clock clock;
+
     while (mWindow.isOpen())
     {
+        sf::Time frameTime = clock.restart();
         processEvents();
-        update();
+        update(frameTime);
         render();
     }
 }
@@ -41,13 +59,50 @@ void Game::processEvents()
 
     while (mWindow.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
-            mWindow.close();
+        switch (event.type)
+        {
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
+            default:
+                break;
+        }
     }
 }
 
-void Game::update()
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
 {
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    else if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    else if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    else if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
+    else if (key == sf::Keyboard::Escape && isPressed)
+        mWindow.close();
+}
+
+void Game::update(sf::Time frameTime)
+{
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= PlayerSpeed;
+    if (mIsMovingDown)
+        movement.y += PlayerSpeed;
+    if (mIsMovingLeft)
+        movement.x -= PlayerSpeed;
+    if (mIsMovingRight)
+        movement.x += PlayerSpeed;
+
+    mPlayer.move(movement * frameTime.asSeconds());
 }
 
 void Game::render()
