17502394c985d7245805e6aec0ab5dd7ac5a6021
Vyacheslav Moskalev <moskalev@wonder-will.com>
2015-03-16 12:59:15 +0600

    Updated title

--- Game.cpp
+++ Game.cpp
@@ -7,7 +7,7 @@
 const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
 
 Game::Game()
-: mWindow(sf::VideoMode(800, 600), "---=#_$ ^ *  crank ` . $_#=---", sf::Style::Close)
+: mWindow(sf::VideoMode(800, 600), "Crank", sf::Style::Close)
 , mWorld(mWindow)
 , mPlayer()
 , mFonts()
