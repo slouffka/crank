255e92c8d6df54adea9fd09ba88a67f1b3429758
Vyacheslav Moskalev <slouffka@gmail.com>
2015-04-10 11:47:43 +0600

    Updated compile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
 
 clang++ -std=c++11 -O3 -lsfml-window -lsfml-graphics -lsfml-system -o crank \
+    Animation.cpp       \
     Application.cpp     \
     Button.cpp          \
+    BloomEffect.cpp     \
     Command.cpp         \
     CommandQueue.cpp    \
     Component.cpp       \
     Container.cpp       \
     DataTables.cpp      \
+    EmitterNode.cpp     \
     Entity.cpp          \
     GameOverState.cpp   \
     GameState.cpp       \
@@ -14,9 +17,11 @@ clang++ -std=c++11 -O3 -lsfml-window -lsfml-graphics -lsfml-system -o crank \
     LoadingState.cpp    \
     MenuState.cpp       \
     ParallelTask.cpp    \
+    ParticleNode.cpp    \
     PauseState.cpp      \
     Pickup.cpp          \
     Player.cpp          \
+    PostEffect.cpp      \
     Projectile.cpp      \
     SceneNode.cpp       \
     SettingsState.cpp   \
